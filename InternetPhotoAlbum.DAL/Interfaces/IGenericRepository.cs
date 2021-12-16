5f2a23cb437f6433bf29dec2c324337da5cf921f
Mykhailo Teziaiev <fusionmike99@gmail.com>
2021-12-16 16:34:06 +0200

    Have added repositories interfaces

---
+++ InternetPhotoAlbum.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetPhotoAlbum.DAL.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> CreateAsync(TEntity entity);
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<bool> DeleteAsync(int id);
+
+    }
+}
