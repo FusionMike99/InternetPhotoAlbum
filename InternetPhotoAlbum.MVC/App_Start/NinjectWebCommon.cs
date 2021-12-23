0187555878fbd5bc0720f33f505f20b1ebae5994
Mykhailo Teziaiev <fusionmike99@gmail.com>
2021-12-23 15:28:51 +0200

    Change config of Ninject V2

--- InternetPhotoAlbum.MVC/App_Start/NinjectWebCommon.cs
+++ InternetPhotoAlbum.MVC/App_Start/NinjectWebCommon.cs
@@ -3,25 +3,21 @@
 
 namespace InternetPhotoAlbum.MVC.App_Start
 {
-    using System;
-    using System.Web;
-
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
     using Ninject;
     using Ninject.Web.Common;
     using Ninject.Web.Common.WebHost;
-    using System.Configuration;
-    using InternetPhotoAlbum.BLL.Infrastructure;
+    using System;
+    using System.Web;
 
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application.
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
@@ -63,9 +59,7 @@ namespace InternetPhotoAlbum.MVC.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            string connectionString = ConfigurationManager.ConnectionStrings["InternetPhotoAlbumDb"].ConnectionString;
-            DataAccessBinding dataAccessBinding = new DataAccessBinding(connectionString);
-            kernel.Load(dataAccessBinding);
+
         }
     }
 }
--- InternetPhotoAlbum.MVC/App_Start/Startup.cs
+++ InternetPhotoAlbum.MVC/App_Start/Startup.cs
@@ -1,13 +1,11 @@
-﻿using InternetPhotoAlbum.BLL.Interfaces;
+﻿using InternetPhotoAlbum.BLL.Infrastructure;
+using InternetPhotoAlbum.BLL.Interfaces;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Ninject;
 using Owin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
 
 [assembly: OwinStartup(typeof(InternetPhotoAlbum.MVC.App_Start.Startup))]
 namespace InternetPhotoAlbum.MVC.App_Start
@@ -17,7 +15,7 @@ namespace InternetPhotoAlbum.MVC.App_Start
         private IKernel kernel = null;
         public void Configuration(IAppBuilder app)
         {
-            kernel = NinjectWebCommon.CreateKernel();
+            kernel = CreateKernel();
             app.UseNinject(() => kernel);
 
             app.CreatePerOwinContext(() => kernel.Get<IUserService>());
@@ -28,6 +26,23 @@ namespace InternetPhotoAlbum.MVC.App_Start
                 LoginPath = new PathString("/Account/Login"),
                 LogoutPath = new PathString("/Account/Logout")
             });
+        }
+
+        public IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["InternetPhotoAlbumDb"].ConnectionString;
+                DataAccessBinding dataAccessBinding = new DataAccessBinding(connectionString);
+                kernel.Load(dataAccessBinding);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
         }
     }
 }
