8a22db3975aa9eab021aad6a07f8c3cd7f802d6e
Mykhailo Teziaiev <fusionmike99@gmail.com>
2022-01-03 13:08:17 +0200

    Implement Code First for Triggers of DELETE

---
+++ InternetPhotoAlbum.DAL/Migrations/202201031051393_TriggersDelete.cs
@@ -0,0 +1,50 @@
+﻿namespace InternetPhotoAlbum.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TriggersDelete : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"CREATE OR ALTER TRIGGER [DELETE_Users]
+                     ON dbo.[AspNetUsers]
+                     INSTEAD OF DELETE
+                  AS 
+                  BEGIN
+                     SET NOCOUNT ON;
+                     DELETE FROM dbo.[UserProfiles] WHERE UserId IN (SELECT Id FROM DELETED)
+                     DELETE FROM dbo.[Ratings] WHERE UserId IN (SELECT Id FROM DELETED)
+                     DELETE FROM dbo.[Albums] WHERE UserId IN (SELECT Id FROM DELETED)
+                     DELETE FROM dbo.[AspNetUsers] WHERE Id IN (SELECT Id FROM DELETED)
+                  END");
+
+            Sql(@"CREATE OR ALTER TRIGGER [DELETE_Albums]
+                     ON dbo.[Albums]
+                     INSTEAD OF DELETE
+                  AS 
+                  BEGIN
+                     SET NOCOUNT ON;
+                     DELETE FROM dbo.[Images] WHERE AlbumId IN (SELECT Id FROM DELETED)
+                     DELETE FROM dbo.[Albums] WHERE Id IN (SELECT Id FROM DELETED)
+                  END");
+
+            Sql(@"CREATE OR ALTER TRIGGER [DELETE_Images]
+                     ON dbo.[Images]
+                     INSTEAD OF DELETE
+                  AS 
+                  BEGIN
+                     SET NOCOUNT ON;
+                     DELETE FROM dbo.[Rating] WHERE ImageId IN (SELECT Id FROM DELETED)
+                     DELETE FROM dbo.[Images] WHERE Id IN (SELECT Id FROM DELETED)
+                  END");
+        }
+
+        public override void Down()
+        {
+            Sql("DROP TRIGGER [DELETE_Images]");
+            Sql("DROP TRIGGER [DELETE_Albums]");
+            Sql("DROP TRIGGER [DELETE_Users]");
+        }
+    }
+}
