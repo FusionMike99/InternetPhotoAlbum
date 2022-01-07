e8886a1bc3128d92b0bd04ddad7bde19f18b98a1
Mykhailo Teziaiev <fusionmike99@gmail.com>
2022-01-07 14:08:34 +0200

    Implement custom errors pages

---
+++ InternetPhotoAlbum.MVC/Controllers/ErrorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace InternetPhotoAlbum.MVC.Controllers
+{
+    /// <summary>
+    /// Controlling application errors
+    /// </summary>
+    public class ErrorController : Controller
+    {
+        /// <summary>
+        /// Error with status code 404 (Not found)
+        /// </summary>
+        /// <returns>Result of action</returns>
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        /// <summary>
+        /// Error with status code 403 (Forbidden)
+        /// </summary>
+        /// <returns>Result of action</returns>
+        public ActionResult Forbidden()
+        {
+            Response.StatusCode = 403;
+            return View();
+        }
+    }
+}
